d9ae2f0d7add683ce5415c45c9e1243ecc34bf05
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-05-20 11:42:39 +0530

    refactored

--- src/main/java/com/bytatech/ayoos/service/impl/QueryServiceImpl.java
+++ src/main/java/com/bytatech/ayoos/service/impl/QueryServiceImpl.java
@@ -155,7 +155,7 @@ public class QueryServiceImpl implements QueryService {
 	List<WorkPlace> workplace =	 elasticsearchTemplate.queryForList(getGeoQuery(point, distance),WorkPlace.class);
 
 	for(WorkPlace wp:workplace ) {
-		System.out.println(">>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<"+wp);
+		System.out.println(">>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<"+wp.getDoctor());
 	}
 	
 	
