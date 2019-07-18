42d934963c244f8b0d7bdfa6ef8bc3a7b8bf1cb5
KarthikeyanPk <karthikeyan.p.k@lxisoft.com>
2019-07-18 10:12:43 +0530

    configuration changed in application

--- src/main/java/com/bytatech/ayoos/client/patient/api/CommandResourceApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/patient/api/CommandResourceApiClient.java
@@ -3,6 +3,6 @@ package com.bytatech.ayoos.client.patient.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.bytatech.ayoos.client.patient.PatientClientConfiguration;
 
-@FeignClient(name="${patient.name:patient}", url="${patient.url:35.184.100.236:8081/}", configuration = PatientClientConfiguration.class)
+@FeignClient(name="${patient.name:patient}", url="${patient.url}", configuration = PatientClientConfiguration.class)
 public interface CommandResourceApiClient extends CommandResourceApi {
 }
