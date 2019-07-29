96337d1db720e5fff4a7d549144b2c82c9a72eb6
abilash-diviso <abilash.s@lxisoft.com>
2019-07-29 13:06:53 +0530

    Clientconfiguration renamed

--- src/main/java/com/bytatech/ayoos/client/payment/api/PaypalQueryResourceApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/payment/api/PaypalQueryResourceApiClient.java
@@ -1,8 +1,8 @@
 package com.bytatech.ayoos.client.payment.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.bytatech.ayoos.client.payment.ClientConfiguration;
+import com.bytatech.ayoos.client.payment.PaymentClientConfiguration;
 
-@FeignClient(name="${payment.name:payment}", url="${payment.url:35.237.4.7:9090/}", configuration = ClientConfiguration.class)
+@FeignClient(name="${payment.name:payment}", url="${payment.url:35.237.4.7:9090/}", configuration = PaymentClientConfiguration.class)
 public interface PaypalQueryResourceApiClient extends PaypalQueryResourceApi {
 }
