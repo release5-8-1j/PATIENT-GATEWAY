09a25e50c77f603a1debaa677123b3cff8549fbd
abilash-diviso <abilash.s@lxisoft.com>
2019-07-30 12:54:40 +0530

    changed appointment URI in clients

--- src/main/java/com/bytatech/ayoos/client/appointment/api/UserResourceApiClient.java
+++ src/main/java/com/bytatech/ayoos/client/appointment/api/UserResourceApiClient.java
@@ -3,6 +3,6 @@ package com.bytatech.ayoos.client.appointment.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.bytatech.ayoos.client.appointment.ClientConfiguration;
 
-@FeignClient(name="${appointment.name:appointment}", url="${appointment.url:35.184.100.236:8090/}", configuration = ClientConfiguration.class)
+@FeignClient(name="${appointment.name:appointment}", url="${appointment.url:34.68.84.96:8090/}", configuration = ClientConfiguration.class)
 public interface UserResourceApiClient extends UserResourceApi {
 }
